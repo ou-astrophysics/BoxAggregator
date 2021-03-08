9c41b304fe3aa0734c3016a46084e69e91affb9b
Hugh Dickinson <hugh.dickinson@open.ac.uk>
2021-03-08 19:21:21 +0000

    Added package explicitly

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
+    packages=["BoxAggregator"],
     install_requires=[
         "numba",
         "numpy",
         "pandas",
         "scipy",
         "FacilityLocation @ git+ssh://git@github.com/ou-escape-eco/FacilityLocation@master",
-    ]
+    ],
 )
