4b0160fcede5cd751cff708d76d5afc17b8409f3
Hugh Dickinson <hugh.dickinson@open.ac.uk>
2021-03-08 19:27:45 +0000

    Added package explicitly

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    packages=["BoxAggregator"],
+    packages=["."],
     install_requires=[
         "numba",
         "numpy",
